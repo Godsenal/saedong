cc25e88c5f36a3f430dee754e7acce077bc20e9d
Jonir227 <bjbj6363@gmail.com>
2017-05-30 00:25:25 +0900

    saedong base

---
+++ producer/actual2.php
@@ -0,0 +1,42 @@
+﻿
+<?
+$suppCode = $_POST["suppCode"]
+$name=$_POST["name"];
+$date=$_POST["date"];
+$material=$_POST["material"];
+
+?>
+
+<?
+set_time_limit(60);
+include "inc_dbconn.inc";
+
+#---------------------------------- 
+# database connect
+#---------------------------------- 
+init_board();
+
+?>
+
+
+
+<?
+#-------- db query -------#
+
+$ins_query = 	"insert into SUPP_INFO (SUPP_CODE, SUPP_REGI_DATE, SUPP_NAME, SUPP_PROC_INFO) 
+				values ('$suppCode','$date','$name','$material')";
+
+$ins_result = mysql_query($ins_query);
+
+if (!$ins_result) { 
+	error_message('DB_ERROR', 'actual2.php', ''); 
+	exit;
+} else {
+echo 	"<script type = 'text/javascript'>
+			alert('공급자 코드 : $suppCode, 공급자이름 : $name, 자재명 : $material, 입력날짜 : $date \n 공급자 $name의 등록이 완료되었습니다.');
+			window.history.back();
+		</script>";
+}
+
+?>
+
---
+++ producer/search.php
@@ -0,0 +1,95 @@
+<html>
+<title>회원정보 조회</title>
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <meta http-equiv="content-type" content=" text/html; charset=utf-8">
+    <link type="text/css" content="width=device-width, initial-scale=1.0" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+</head>
+<body>
+<div id ="container">
+  <?
+  #----------------------------------
+  # database connect
+  #----------------------------------
+  set_time_limit(60);
+  include "inc_dbconn.inc";
+  init_board();
+  #------------------------------------
+  ?>
+
+  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+  <script type = "text/javascript">
+    function approve(itemkey){
+      <?php
+        $ins_query = "select * PR_SUPPLIER approval"
+      ?>
+    }
+  </script>
+  <table class="responsive-table bordered" >
+    <thead style=" background-color:#333333; color:white">
+      <tr>
+        <td>
+          Supplier Code
+        </td>
+        <td>
+          공급자 명
+        </td>
+        <td>
+          자재 명
+        </td>
+        <td>
+          연락처
+        </td>
+        <td>
+          작성일
+        </td>
+        <td>
+          승인여부
+        </td>
+        <td>
+          승인
+        </td>
+      </tr>
+    </thead>
+    <tbody>
+  <?
+    #----------------------------------
+    # Select DB Query
+    #----------------------------------
+    $connect = mysql_connect($dbhost, $dbusername, $dbuserpassword);
+    $other_que="select * from PR_SUPPLIER order by name";
+    $other_result=mysql_query($other_que,$connect);
+    $total = mysql_affected_rows();
+    for($i = 0; $i < $total; $i++) {
+      $code_data = mysql_fetch_row($other_result);
+      $supplier_code = $code_data[0];
+      $name = $code_data[1];
+      $material = $code_data[2];
+      $tel = $code_data[3];
+      $date = $code_data[4];
+      $approval = $code_data[5];
+      $isApproved = 'X';
+      $approvBtn = ' - ';
+      if($approval)
+        $isApproved  = 'O';
+        $approvBtn = '<button type = "button" onclick = "approve($i)">승인</button>';
+      echo ("
+        <tr>
+          <td>$supplier_code</td>
+          <td>$name</td>
+          <td>$material</td>
+          <td>$tel</td>
+          <td>$date</td>
+          <td>$isApproved</td>
+          <td>$approvBtn</td>
+      ");
+      echo ("</tr>");
+    }
+  ?>
+  </tbody>
+  </table>
+</div>
+</body>
+</html>
